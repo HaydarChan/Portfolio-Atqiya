6f8e6729d6110e51ff6513b3614ae19b8322b14c
HaydarChan <atqiyahaydar15@gmail.com>
2025-09-17 14:53:49 +0700

    feat: add mockup data

---
+++ src/lib/Mockups.js
@@ -0,0 +1,169 @@
+// Hentii
+import Hentii1 from "@/assets/mockup/hentii/Beranda.png";
+import Hentii2 from "@/assets/mockup/hentii/Community.png";
+import Hentii3 from "@/assets/mockup/hentii/Fakta Medis.png";
+import Hentii4 from "@/assets/mockup/hentii/Home.png";
+import Hentii5 from "@/assets/mockup/hentii/Journal Merokok.png";
+import Hentii6 from "@/assets/mockup/hentii/Jurnal 1.png";
+import Hentii7 from "@/assets/mockup/hentii/Jurnal 2.png";
+import Hentii8 from "@/assets/mockup/hentii/Kalendar.png";
+import Hentii9 from "@/assets/mockup/hentii/Komitmen.png";
+import Hentii10 from "@/assets/mockup/hentii/Login.png";
+import Hentii11 from "@/assets/mockup/hentii/Pendaftaran 1.png";
+import Hentii12 from "@/assets/mockup/hentii/Peringatan.png";
+import Hentii13 from "@/assets/mockup/hentii/Profile.png";
+import Hentii14 from "@/assets/mockup/hentii/Saran.png";
+import Hentii15 from "@/assets/mockup/hentii/Signup.png";
+import Hentii16 from "@/assets/mockup/hentii/SOS Splash.png";
+import Hentii17 from "@/assets/mockup/hentii/Welcome 1.png";
+import Hentii18 from "@/assets/mockup/hentii/Welcome 4.png";
+// Obatku
+import Obatku1 from "@/assets/mockup/obatku/1.png";
+import Obatku2 from "@/assets/mockup/obatku/2.png";
+import Obatku3 from "@/assets/mockup/obatku/3.png";
+import Obatku4 from "@/assets/mockup/obatku/4.png";
+import Obatku5 from "@/assets/mockup/obatku/5.png";
+import Obatku6 from "@/assets/mockup/obatku/Chat 1.png";
+import Obatku7 from "@/assets/mockup/obatku/Daftar 1.png";
+import Obatku8 from "@/assets/mockup/obatku/Default View.png";
+import Obatku9 from "@/assets/mockup/obatku/Final State.png";
+import Obatku10 from "@/assets/mockup/obatku/Jadwal 1.png";
+import Obatku11 from "@/assets/mockup/obatku/Landing Page 2.png";
+import Obatku12 from "@/assets/mockup/obatku/Landing Page-1.png";
+import Obatku13 from "@/assets/mockup/obatku/Landing Page.png";
+import Obatku14 from "@/assets/mockup/obatku/Landing.png";
+import Obatku15 from "@/assets/mockup/obatku/Obat.png";
+import Obatku16 from "@/assets/mockup/obatku/Penyakit 1.png";
+import Obatku17 from "@/assets/mockup/obatku/Penyakit 2.png";
+import Obatku18 from "@/assets/mockup/obatku/Penyakit 4.png";
+import Obatku19 from "@/assets/mockup/obatku/Tambah Obat Baru 6.png";
+import Obatku20 from "@/assets/mockup/obatku/Tambah Obat Baru 8.png";
+import Obatku21 from "@/assets/mockup/obatku/Tambah Obat Baru.png";
+import Obatku22 from "@/assets/mockup/obatku/View Item.png";
+import Obatku23 from "@/assets/mockup/obatku/Welcome Screen 1.png";
+import Obatku24 from "@/assets/mockup/obatku/Welcome Screen 2.png";
+import Obatku25 from "@/assets/mockup/obatku/Welcome Screen 3.png";
+// EcoTrail
+import Ecotrail1 from "@/assets/mockup/ecotrail/[START] Beranda.png";
+import Ecotrail2 from "@/assets/mockup/ecotrail/[START] Emission Tracker.png";
+import Ecotrail3 from "@/assets/mockup/ecotrail/[START] Jadwal.png";
+import Ecotrail4 from "@/assets/mockup/ecotrail/[START] Komunitas-1.png";
+import Ecotrail5 from "@/assets/mockup/ecotrail/[START] Komunitas.png";
+import Ecotrail6 from "@/assets/mockup/ecotrail/Beranda-1.png";
+import Ecotrail7 from "@/assets/mockup/ecotrail/Beranda-2.png";
+import Ecotrail8 from "@/assets/mockup/ecotrail/Beranda-3.png";
+import Ecotrail9 from "@/assets/mockup/ecotrail/Beranda.png";
+import Ecotrail10 from "@/assets/mockup/ecotrail/Emission Tracker.png";
+import Ecotrail11 from "@/assets/mockup/ecotrail/Jadwal.png";
+import Ecotrail12 from "@/assets/mockup/ecotrail/Komunitas-1.png";
+import Ecotrail13 from "@/assets/mockup/ecotrail/Komunitas.png";
+import Ecotrail14 from "@/assets/mockup/ecotrail/Landing-1.png";
+import Ecotrail15 from "@/assets/mockup/ecotrail/Landing-2.png";
+import Ecotrail16 from "@/assets/mockup/ecotrail/Landing-3.png";
+import Ecotrail17 from "@/assets/mockup/ecotrail/Landing.png";
+import Ecotrail18 from "@/assets/mockup/ecotrail/Template-1.png";
+import Ecotrail19 from "@/assets/mockup/ecotrail/Template-2.png";
+import Ecotrail20 from "@/assets/mockup/ecotrail/Template.png";
+// Ainstein
+import Ainstein1 from "@/assets/mockup/ainstein/Generate - Flash Card.png";
+import Ainstein2 from "@/assets/mockup/ainstein/Generate - Menu Opened.png";
+import Ainstein3 from "@/assets/mockup/ainstein/Generate - Quiz.png";
+import Ainstein4 from "@/assets/mockup/ainstein/Generate - Video.png";
+import Ainstein5 from "@/assets/mockup/ainstein/Generate-1.png";
+import Ainstein6 from "@/assets/mockup/ainstein/Generate-2.png";
+import Ainstein7 from "@/assets/mockup/ainstein/Generate.png";
+import Ainstein8 from "@/assets/mockup/ainstein/Learn - Comment Opened.png";
+import Ainstein9 from "@/assets/mockup/ainstein/Learn.png";
+import Ainstein10 from "@/assets/mockup/ainstein/Login.png";
+import Ainstein11 from "@/assets/mockup/ainstein/Study Kit - Video.png";
+import Ainstein12 from "@/assets/mockup/ainstein/Study Kit-1.png";
+import Ainstein13 from "@/assets/mockup/ainstein/Study Kit.png";
+// Purritfy
+import Purritify1 from "@/assets/mockup/purritify/Display Music.png";
+import Purritify2 from "@/assets/mockup/purritify/Home.png";
+import Purritify3 from "@/assets/mockup/purritify/LogIn.png";
+import Purritify4 from "@/assets/mockup/purritify/Profile.png";
+import Purritify5 from "@/assets/mockup/purritify/Your Library Add Song.png";
+import Purritify6 from "@/assets/mockup/purritify/Your Library.png";
+// Trackure
+import Trackure1 from "@/assets/mockup/trackure/Admin - Daftar Pengadaan.png";
+import Trackure2 from "@/assets/mockup/trackure/Admin - Daftar Permohonan-1.png";
+import Trackure3 from "@/assets/mockup/trackure/Admin - Daftar Permohonan.png";
+import Trackure4 from "@/assets/mockup/trackure/Admin - Tahap Pengajuan Case 0.png";
+import Trackure5 from "@/assets/mockup/trackure/Admin - Tahap Pengajuan Case 1-1.png";
+import Trackure6 from "@/assets/mockup/trackure/Admin - Tahap Pengajuan Case 1.png";
+import Trackure7 from "@/assets/mockup/trackure/Admin - Tahap Pengiriman Barang.png";
+import Trackure8 from "@/assets/mockup/trackure/Admin.png";
+import Trackure9 from "@/assets/mockup/trackure/Super Admin - Log Aktivitas.png";
+import Trackure10 from "@/assets/mockup/trackure/Super Admin - Pengguna.png";
+import Trackure11 from "@/assets/mockup/trackure/User Kaprodi - Dasbor.png";
+import Trackure12 from "@/assets/mockup/trackure/User Kaprodi - Tracker Barang-1.png";
+import Trackure13 from "@/assets/mockup/trackure/User Kaprodi - Tracker Barang.png";
+import Trackure14 from "@/assets/mockup/trackure/User Print Number.png";
+
+export const mockupData = [
+ {
+    label: "Hentii",
+    mockup: [
+        Hentii1,
+        Hentii2,
+        Hentii3,
+        Hentii4,
+        Hentii5,
+        Hentii6,
+        Hentii7,
+        Hentii8,
+        Hentii9,
+        Hentii10,
+        Hentii11,
+        Hentii12,
+        Hentii13,
+        Hentii14,
+        Hentii15,
+        Hentii16,
+        Hentii17,
+        Hentii18,
+    ]      
+ },
+ {
+    label: "Obatku",
+    mockup: [
+        Obatku1, Obatku2, Obatku3, Obatku4, Obatku5,
+        Obatku6, Obatku7, Obatku8, Obatku9, Obatku10,
+        Obatku11, Obatku12, Obatku13, Obatku14, Obatku15,
+        Obatku16, Obatku17, Obatku18, Obatku19, Obatku20,
+        Obatku21, Obatku22, Obatku23, Obatku24, Obatku25,
+    ]
+ },
+ {
+    label: "EcoTrail",
+    mockup: [
+        Ecotrail1, Ecotrail2, Ecotrail3, Ecotrail4, Ecotrail5,
+        Ecotrail6, Ecotrail7, Ecotrail8, Ecotrail9, Ecotrail10,
+        Ecotrail11, Ecotrail12, Ecotrail13, Ecotrail14, Ecotrail15,
+        Ecotrail16, Ecotrail17, Ecotrail18, Ecotrail19, Ecotrail20,
+      ]
+ },
+ {
+    label: "Ainstein",
+    mockup: [
+        Ainstein1, Ainstein2, Ainstein3, Ainstein4, Ainstein5,
+        Ainstein6, Ainstein7, Ainstein8, Ainstein9, Ainstein10,
+        Ainstein11, Ainstein12, Ainstein13,
+      ]
+ },
+ {
+    label: "Purritify",
+    mockup: [
+        Purritify1, Purritify2, Purritify3, Purritify4, Purritify5, Purritify6,
+      ]
+ },
+ {
+    label: "Trackure",
+    mockup: [
+        Trackure1, Trackure2, Trackure3, Trackure4, Trackure5,
+        Trackure6, Trackure7, Trackure8, Trackure9, Trackure10,
+        Trackure11, Trackure12, Trackure13, Trackure14,
+      ]
+ },
+]
